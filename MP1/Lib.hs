408e0dc448344f474412b4cd451e688fe5a5b0d9
DexinRen <deshaun.ren@gmail.com>
2022-06-12 06:24:29 +0000

    upload note

---
+++ MP1/Lib.hs
@@ -0,0 +1,9 @@
+module Lib where
+data List a = Cons a (List a)
+            | Nil
+  deriving (Show, Eq)
+
+data Exp = IntExp Integer
+         | PlusExp [Exp]
+         | MultExp [Exp]
+  deriving (Show, Eq)
---
+++ MP2/Lib.hs
@@ -0,0 +1,78 @@
+module Lib where
+import Data.HashMap.Strict as H (HashMap, empty, fromList, insert, lookup, union)
+
+
+--- Data Types
+--- ----------
+
+--- ### Environments and Results
+
+type Env  = H.HashMap String Val
+type PEnv = H.HashMap String Stmt
+
+type Result = (String, PEnv, Env)
+
+--- ### Values
+
+data Val = IntVal Int
+         | BoolVal Bool
+         | CloVal [String] Exp Env
+         | ExnVal String
+    deriving (Eq)
+
+instance Show Val where
+    show (IntVal i) = show i
+    show (BoolVal i) = show i
+    show (CloVal xs body env) = "<" ++ show xs   ++ ", "
+                                    ++ show body ++ ", "
+                                    ++ show env  ++ ">"
+    show (ExnVal s) = "exn: " ++ s
+
+--- ### Expressions
+
+data Exp = IntExp Int
+         | BoolExp Bool
+         | FunExp [String] Exp
+         | LetExp [(String,Exp)] Exp
+         | AppExp Exp [Exp]
+         | IfExp Exp Exp Exp
+         | IntOpExp String Exp Exp
+         | BoolOpExp String Exp Exp
+         | CompOpExp String Exp Exp
+         | VarExp String
+    deriving (Show, Eq)
+
+--- ### Statements
+
+data Stmt = SetStmt String Exp
+          | PrintStmt Exp
+          | QuitStmt
+          | IfStmt Exp Stmt Stmt
+          | ProcedureStmt String [String] Stmt
+          | CallStmt String [Exp]
+          | SeqStmt [Stmt]
+    deriving (Show, Eq)
+
+--- Primitive Functions
+--- -------------------
+
+intOps :: H.HashMap String (Int -> Int -> Int)
+intOps = H.fromList [ ("+", (+))
+                    , ("-", (-))
+                    , ("*", (*))
+                    , ("/", (div))
+                    ]
+
+boolOps :: H.HashMap String (Bool -> Bool -> Bool)
+boolOps = H.fromList [ ("and", (&&))
+                     , ("or", (||))
+                     ]
+
+compOps :: H.HashMap String (Int -> Int -> Bool)
+compOps = H.fromList [ ("<", (<))
+                     , (">", (>))
+                     , ("<=", (<=))
+                     , (">=", (>=))
+                     , ("/=", (/=))
+                     , ("==", (==))
+                     ]
